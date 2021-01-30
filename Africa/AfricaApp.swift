4498dba4a81b4fe19163ec5f4d438d935065c30c
Eric M. Seitz <eric.seitz@gmail.com>
2021-01-30 09:52:35 -0800

    Initial commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Eric Seitz on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
