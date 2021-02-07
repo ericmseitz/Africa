db4e57c400944ea221671e56574d29ff39061407
Eric M. Seitz <eric.seitz@gmail.com>
2021-02-07 12:30:32 -0800

    create CoverImageModel

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Eric Seitz on 2/7/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
