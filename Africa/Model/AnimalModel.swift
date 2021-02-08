fd01eeeac8fa0a4f3c364c314204e94cfadf52e1
Eric M. Seitz <eric.seitz@gmail.com>
2021-02-08 08:57:35 -0800

    create animal list item view and ustilize

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Eric Seitz on 2/8/21.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
