ead129c7f4556d20ab3e13c8a33240c9e81af515
Eric M. Seitz <eric.seitz@gmail.com>
2021-03-04 12:35:46 -0800

    develop detail view for each animal, with dynamic content.

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -17,11 +17,37 @@ struct AnimalDetailView: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
                 // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
                 
                 // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
                 
                 // GALLERY
-                
+                Group {
+                HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
                 // FACTS
                 
                 // DESCRIPTION
@@ -31,7 +57,7 @@ struct AnimalDetailView: View {
                 // LINK
                 
             } //: VSTACK
-            .navigationBarTitle("Learn about \(animal.name)",
+            .navigationBarTitle("Learn about the \(animal.name)",
                                 displayMode: .inline)
         } //: SCROLL
     }
